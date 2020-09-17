0628f36e1ad34deff8ff15c3d6169a7b000461ba
Jon Otaegi <jonotaegi@gmail.com>
2020-09-17 11:15:16 +0200

    Added database connection check.

--- files/health.php
+++ files/health.php
@@ -1,1 +1,13 @@
-<?php header("Content-length: 0"); ?>
+<?php
+require_once("wp-load.php");
+
+// Use the global instance created by WordPress
+global $wpdb;
+
+// Check the connection:
+if(!$wpdb->check_connection($allow_bail = false)) {
+    die(__("No DB connection"));
+}
+
+header("Content-length: 0");
+?>
